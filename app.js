9b1ed12b678baa6bd950620b794d98b159270359
Ritesh Jangra <riteshk2874@gmail.com>
2021-02-10 00:08:21 +0530

    final edit1.0

---
+++ app.js
@@ -0,0 +1,6 @@
+$("button").on("click", function () {
+  $(".second-person").toggleClass("visible");
+  $(".image2").toggleClass("visible");
+  $(".image1").toggleClass("visible");
+  $(".first-person").toggleClass("visible");
+})
